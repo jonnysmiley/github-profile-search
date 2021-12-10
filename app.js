9fabf59001efaf5137f277a9c2c8cb00ebf5e596
Jonathan Smiley <92873891+jonnysmiley@users.noreply.github.com>
2021-12-10 14:38:36 -0800

    Create app.js

---
+++ app.js
@@ -0,0 +1,30 @@
+//init Github
+const github = new Github;
+//init UI 
+const ui = new UI;
+//search inpuut
+
+const searchUser = document.getElementById("searchUser");
+
+//search input event listener 
+searchUser.addEventListener("keyup", (e) => {
+    //get input text
+    const userText = e.target.value;
+
+    if(userText !== ''){
+        github.getUser(userText)
+        .then(data => {
+            if(data.profile.message === "Not Found") {
+                //show alert
+                ui.showAlert("User not found", "alert alert-danger");
+            } else {
+                //show profile 
+                ui.showProfile(data.profile);
+                ui.showRepos(data.repos);
+            }
+        })
+    } else {
+        //clear profile 
+        ui.clearProfile();
+    }
+}); 
